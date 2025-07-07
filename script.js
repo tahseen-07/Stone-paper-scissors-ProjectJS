e59f51a714d831e5d61a29e44240ed8f46388aa1
tahseen-07 <155661622+tahseen-07@users.noreply.github.com>
2025-07-07 12:53:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice ")
+
+const msg = document.querySelector("#msg");
+
+const userScorePara =  document.querySelector("#user-score");
+const compScorePara =  document.querySelector("#comp-score");
+
+const genCompChoice = () =>{
+    const options = ["rock", "paper", "scissors"];
+    const randomIdx = Math.floor(Math.random() * 3 );
+    return options[randomIdx];
+};
+
+const showWinner = (userWin, userChoice, compChoice) =>{
+    if(userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You Win your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    } 
+    else{
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You loose ${compChoiceChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const drawGame = () =>{
+    console.log("Game was Draw");
+    msg.innerText = "Game was draw. Play again ";
+
+};
+
+const playGame = (userChoice) =>{
+
+    const compChoice = genCompChoice();
+
+
+    if(userChoice === compChoice){
+        // Draw Game
+        drawGame();
+    }else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "paper" ? false : true ;
+        }else if(userChoice === "paper"){
+            userWin = compChoice === "scissors" ? false : true ;
+        }else{
+            userWin = compChoice === "rock" ? false : true ;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+};
+
+choices.forEach((choice) => {
+    console.log(choice);
+    choice.addEventListener("click", () =>{
+        const userChoice = choice.getAttribute("id")
+        // console.log("choice was clicked",userChoice);
+        playGame(userChoice);
+    });
+});
